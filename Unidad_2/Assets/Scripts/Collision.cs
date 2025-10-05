7e443aa5e9aa559fc39c20086a50245fbac4220b
Zabdiel Soto <zabdiel.soto24a@udgvirtual.udg.mx>
2025-10-05 23:13:48 +1100

    actividad5

---
+++ Unidad_2/Assets/Scripts/Collision.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public TextMeshProUGUI buttonText; // Or public Text buttonText; for regular UI Text
+    public TMP_FontAsset font1; // Or public Font font1;
+    public TMP_FontAsset font2; // Or public Font font2;
+
+    private bool isFont1Active = true;
+    void OnTriggerEnter2D (Collider2D coll) {
+        Debug.Log("Collition detected");
+
+        if (isFont1Active)
+        {
+            buttonText.font = font2;
+        }
+        else
+        {
+            buttonText.font = font1;
+        }
+        isFont1Active = !isFont1Active; // Toggle the state
+    }
+}
